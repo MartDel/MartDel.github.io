fd53cc774c118dcfa7486f96687a9943dce7054e
Martin Delebecque <martin-delebecque@outlook.fr>
2021-05-16 17:22:53 +0200

    Update items.js

--- items.js
+++ items.js
@@ -1,14 +1,92 @@
 const items = [
-    { name: "item1", recipe: null },
+    {name: 'Echantillon de vigne d\'orin', recipe: null},
+    {name: 'Graine de vigne d\'orin', recipe: null},
+    {name: 'Qwartz', recipe: null},
+    {name: 'Cuivre', recipe: null},
+    {name: 'Argent', recipe: null},
+    {name: 'Titane', recipe: null},
+    {name: 'Soufre cristallin', recipe: null},
+    {name: 'Plante à rubans', recipe: null},
     {
-        name: "item2",
+        name: 'Maille de fibre',
         recipe: [
-            {name: 'item1', amount: 2},
+            {name: 'Echantillon de vigne d\'orin', amount: 2}
         ]
     },{
-        name: "item3",
+        name: 'Caoutchouc en silicone',
         recipe: [
-            {name: "item2", amount: 2},
+            {name: 'Graine de vigne d\'orin', amount: 1}
         ]
-    },
+    },{
+        name: 'Verre',
+        recipe: [
+            {name: 'Qwartz', amount: 2}
+        ]
+    },{
+        name: 'Fil de cuivre',
+        recipe: [
+            {name: 'Cuivre', amount: 2}
+        ]
+    },{
+        name: 'Pile',
+        recipe: [
+            {name: 'Cuivre', amount: 1},
+            {name: 'Plante à rubans', amount: 2}
+        ]
+    },{
+        name: 'Kit de cablage',
+        recipe: [
+            {name: 'Argent', amount: 2}
+        ]
+    },{
+        name: 'Réservoir d\'O2 standard',
+        recipe: [
+            {name: 'Titane', amount: 2},
+            {name: 'Maille de fibre', amount: 1}
+        ]
+    },{
+        name: 'Palmes',
+        recipe: [
+            {name: 'Caoutchouc en silicone', amount: 2}
+        ]
+    },{
+        name: 'Kit de soin',
+        recipe: [
+            {name: 'Maille de fibre', amount: 1}
+        ]
+    },{
+        name: 'Boussole',
+        recipe: [
+            {name: 'Fil de fibre', amount: 1},
+            {name: 'Kit de cablage', amount: 1}
+        ]
+    },{
+        name: 'Scanner',
+        recipe: [
+            {name: 'Pile', amount: 1},
+            {name: 'Titane', amount: 1}
+        ]
+    },{
+        name: 'Lampe de poche',
+        recipe: [
+            {name: 'Pile', amount: 1},
+            {name: 'Verre', amount: 1}
+        ]
+    },{
+        name: 'Lampe de poche',
+        recipe: [
+            {name: 'Caoutchouc en silicone', amount: 1},
+            {name: 'Titane', amount: 1}
+        ]
+    },{
+        name: 'Fusée éclairante',
+        recipe: [
+            {name: 'Soufre cristallin', amount: 1}
+        ]
+    },{
+        name: 'Casier étanche',
+        recipe: [
+            {name: 'Titane', amount: 4}
+        ]
+    }
 ]
